092c5eea9de9309c7a37ddad4f54346fa9097972
haileemiu <39972604+haileemiu@users.noreply.github.com>
2021-03-14 13:07:05 -0500

    edit and update

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,6 +7,7 @@ use App\Models\Article;
 
 class ArticlesController extends Controller
 {
+    // list all data
     public function index()
     {
         $articles = Article::latest()->get();
@@ -14,6 +15,7 @@ class ArticlesController extends Controller
         return view('articles.index', ['articles' => $articles]);
     }
 
+    // show 1 data item
     public function show($id)
     {
         $article = Article::find($id);
@@ -21,18 +23,46 @@ class ArticlesController extends Controller
         return view('articles.show', ['article' => $article]);
     }
 
+    // Create new data
     public function create()
     {
         return view('articles.create');
     }
 
+    // Persist that ^ new data
     public function store()
     {
+        // request()->validate([
+        //     'title'=>'required|min:3|max:255',
+        //     'excerpt'=>'required',
+        //     'body'=>'required'
+        // ]);
+
         $article = new Article();
         $article->title = request('title');
         $article->excerpt = request('excerpt');
         $article->body = request('body');
         $article->save();
         return redirect('/articles');
+    }
+
+    // Change the data
+    public function edit($id)
+    {
+        $article = Article::find($id);
+        return view('articles.edit', ['article' => $article]);
+    }
+
+    // Persist the change ^
+    public function update($id)
+    {
+        $article = Article::find($id);
+
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+        $article->save();
+
+        return redirect('/articles/' . $article->id);
     }
 }
